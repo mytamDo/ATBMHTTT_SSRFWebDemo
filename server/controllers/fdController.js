afe947391c75614d6e93e77e3aa5f98a756d4f1c
Phong Trần Thạnh <20521750@ms.uit.edu.vn>
2022-11-14 15:06:59 +0700

    add footer and fix

--- server/controllers/fdController.js
+++ server/controllers/fdController.js
@@ -3,7 +3,10 @@ require('../models/database');
 const User = require('../models/User');
 const Role = require('../models/Role')
 
-
+/**
+ * GET /
+ * Homepage
+ */
 exports.homepage = async(req, res) => {
     try{
         const users = await User.find({})
@@ -13,19 +16,47 @@ exports.homepage = async(req, res) => {
     }
 }
 
+/**
+ * GET /login
+ * login
+ */
 exports.login = async(req, res) => {
-    try{
-        res.render('login'), {title: 'F&D - Login'}
-    }catch(error){
-        res.status(500).send({message: error.message || "Error Occured"})
-    }
+  try{
+      res.render('login', {title: 'F&D - Login'})
+  }catch(error){
+      res.status(500).send({message: error.message || "Error Occured"})
+  }
 }
+/**
+ * POST
+ * login on post
+ */
+exports.loginOnPost =  async(req, res) =>{
+  try {
+    var email = req.body.email;
+    var pass = req.body.password;
+    var user = await User.findOne({
+      _email: email, 
+      _pass: pass
+    });
+    console.log(user);
+    res.json(user);
+
+  } catch (error) {
+    res.redirect('/login');
+  }
+}
+
+/**
+ * GET /register
+ * register
+ */
 exports.register = async(req, res) =>{
-    try{
-        res.render('register'), {title: 'F&D - Register'}
-    }catch(error){
-        res.status(500).send({message: error.message || "Error Occured"})
-    }
+  try{
+      res.render('register'), {title: 'F&D - Register'}
+  }catch(error){
+      res.status(500).send({message: error.message || "Error Occured"})
+  }
 }
 
 async function insertDymmyUserData(){
@@ -41,23 +72,24 @@ async function insertDymmyUserData(){
       console.log('err', + error)
     }
   }
-  async function insertDymmyRoleData(){
-    try {
-      await Role.insertMany([
-          { 
-            role: "ADM"
-          },
-          {
-            role: "SHI"
-          },
-          {
-            role: "USR"
-          }
-      ]);
-    } catch (error) {
-      console.log('err', + error)
-    }
+
+async function insertDymmyRoleData(){
+  try {
+    await Role.insertMany([
+        { 
+          role: "ADM"
+        },
+        {
+          role: "SHI"
+        },
+        {
+          role: "USR"
+        }
+    ]);
+  } catch (error) {
+    console.log('err', + error)
   }
+}
 
 
 // insertDymmyUserData();
--- server/routes/fdRoutes.js
+++ server/routes/fdRoutes.js
@@ -5,5 +5,6 @@ const fdController = require('../controllers/fdController');
 router.get('/',fdController.homepage);
 router.get('/login',fdController.login);
 router.get('/register',fdController.register)
+router.post('/login',fdController.loginOnPost)
 
 module.exports = router;
